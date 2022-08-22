081244ce9c3996f059b36b32694cc4ae72f8d08d
Lingga <linggaadiatmdj@gmail.com>
2022-08-22 23:11:37 +0700

    feat: about app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:jajanan_boeng/pages/app_info_page.dart';
+import 'package:jajanan_boeng/pages/transaction_page.dart';
+import 'package:jajanan_boeng/providers/logout_provider.dart';
+import 'package:jajanan_boeng/providers/transaction_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:jajanan_boeng/pages/cart_page.dart';
 import 'package:jajanan_boeng/pages/checkout_page.dart';
 import 'package:jajanan_boeng/pages/checkout_success_page.dart';
-import 'package:jajanan_boeng/pages/detail_chat_page.dart';
 import 'package:jajanan_boeng/pages/edit_profile_page.dart';
 import 'package:jajanan_boeng/pages/change_password_page.dart';
 import 'package:jajanan_boeng/pages/home/main_page.dart';
-import 'package:jajanan_boeng/pages/product_page.dart';
 import 'package:jajanan_boeng/pages/sign_in_page.dart';
 import 'package:jajanan_boeng/pages/sign_up_page.dart';
 import 'package:jajanan_boeng/pages/splash_page.dart';
@@ -16,7 +18,7 @@ import 'package:jajanan_boeng/providers/auth_provider.dart';
 import 'package:jajanan_boeng/providers/cart_provider.dart';
 import 'package:jajanan_boeng/providers/page_provider.dart';
 import 'package:jajanan_boeng/providers/product_provider.dart';
-import 'package:jajanan_boeng/providers/transaction_provider.dart';
+import 'package:jajanan_boeng/providers/checkout_provider.dart';
 import 'package:jajanan_boeng/providers/wishlist_provider.dart';
 
 void main() async {
@@ -34,6 +36,9 @@ class MyApp extends StatelessWidget {
           create: (context) => AuthProvider(),
         ),
         ChangeNotifierProvider(
+          create: (context) => LogoutProvider(),
+        ),
+        ChangeNotifierProvider(
           create: (context) => ProductProvider(),
         ),
         ChangeNotifierProvider(
@@ -41,6 +46,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (context) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CheckoutProvider(),
         ),
         ChangeNotifierProvider(
           create: (context) => TransactionProvider(),
@@ -53,12 +61,14 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         routes: {
           '/': (context) => SplashPage(),
+          '/info': (context) => AppInfoPage(),
           '/sign-in': (context) => SignInPage(),
           '/sign-up': (context) => SignUpPage(),
           '/home': (context) => MainPage(),
           '/edit-profile': (context) => EditProfilePage(),
           '/change-password': (context) => ChangePasswordPage(),
           '/cart': (context) => CartPage(),
+          '/transactions': (context) => TransactionPage(),
           '/checkout': (context) => CheckoutPage(),
           '/checkout-success': (context) => CheckoutSuccessPage(),
         },
---
+++ lib/pages/app_info_page.dart
@@ -0,0 +1,124 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:jajanan_boeng/pages/sign_in_page.dart';
+import 'package:jajanan_boeng/theme.dart';
+
+class AppInfoPage extends StatelessWidget {
+  const AppInfoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        padding: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: defaultMargin,
+        ),
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+          ),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Selamat Datang di Jajanan Bo'eng",
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget appinfo() {
+      return Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: defaultMargin,
+            ),
+            Container(
+              // margin: EdgeInsets.only(left: 30, right: 30),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(25.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/rumah-boeng.jpg",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: defaultMargin,
+            ),
+            Text(
+              'Jajanan Bo’eng merupakan usaha kuliner yang menjual makanan khas Anambas yaitu Luti Gendang. Luti gendang merupakan roti yang dibuat dari olahan tepung terigu, ragi, margarin, susu cair, telur ayam, gula, dan garam sebagai adonan utama dengan isian ikan tuna.',
+              style: subtitleTextStyle,
+              textAlign: TextAlign.justify,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget loginPageButton() {
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/sign-in');
+          },
+          style: TextButton.styleFrom(
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            'Login atau Register Sekarang',
+            style: whiteTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor7,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [header(), appinfo(), loginPageButton()],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
